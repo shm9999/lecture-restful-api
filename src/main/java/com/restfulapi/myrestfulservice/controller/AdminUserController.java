1217eb43127412effd5265383f6b1c1565accb17
shm9999 <shm9856@gmail.com>
2025-07-01 15:52:03 +0900

    feat: 응답값 필터링(JsonFilter) - 동적 필터링

---
+++ src/main/java/com/restfulapi/myrestfulservice/controller/AdminUserController.java
@@ -0,0 +1,73 @@
+package com.restfulapi.myrestfulservice.controller;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.restfulapi.myrestfulservice.bean.AdminUser;
+import com.restfulapi.myrestfulservice.bean.User;
+import com.restfulapi.myrestfulservice.dao.UserDaoService;
+import com.restfulapi.myrestfulservice.exception.UserNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsersForAdmin() {
+        List<User> users = service.findAll();
+        List<AdminUser> adminUsers = new ArrayList<>();
+
+        AdminUser adminUser = null;
+        for (User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUserForAdmin(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+}
