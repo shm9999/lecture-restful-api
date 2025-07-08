92d0e03e1d7e0cbd3ab65bc838f220ddb99c4983
shm9999 <shm9856@gmail.com>
2025-07-08 11:11:21 +0900

    feat: Spring Data JPA 를 이용한 초기 데이터 생성 - h2-console 과 spring security 동시 사용을 위한 세팅 포함

--- src/main/java/com/restfulapi/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/com/restfulapi/myrestfulservice/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.restfulapi.myrestfulservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -29,4 +31,10 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // h2-console 과 spring security 동시 사용을 위한 세팅
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111', '711010-1111111');
+insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test222', '721114-1111111');
+insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test333', '731215-1111111');
